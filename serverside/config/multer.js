cef57e8580df0dc8d4efab35e8827a5b901bc4f1
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-01-27 00:33:38 +0100

    music app backend

---
+++ serverside/config/multer.js
@@ -0,0 +1,34 @@
+let multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  console.log(file.mimetype)
+
+  if (
+    file.mimetype === "audio/mpeg" ||
+    file.mimetype === "audio/wave" ||
+    file.mimetype === "audio/wav"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+
+exports.upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
